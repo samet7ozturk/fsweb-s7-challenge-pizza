21fd6b1426823527a359d2375ce91a6cbb55c30a
samet7ozturk <sametozturk973@gmail.com>
2023-11-05 12:07:57 +0300

    Import css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./SuccessPage.css";
+
 const HomePage = () => {
   return (
-    <>
+    <div>
       <h1>Teknolojik Yemekler</h1>
       <p>fırsatı kaçırma</p>
       <h2>KOD ACIKTIRIR</h2>
       <h2>PİZZA DOYURUR</h2>
       <Link to="/order-page">ACIKTIM</Link>
-    </>
+    </div>
   );
 };
 
